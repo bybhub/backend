fbdb9ba23e27bc22083506f1f3bc8003ee921e3a
João Gabriel Soares <joao.soares@inchurch.com.br>
2025-02-23 01:20:49 -0300

    add db connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,16 @@ import (
 	"github.com/bybhub/backend/internal/routes"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
-	config.InitializeSecrets()
+	logger = *config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config init error: %s", err)
+		return
+	}
 	routes.InitializeRouter()
 }
